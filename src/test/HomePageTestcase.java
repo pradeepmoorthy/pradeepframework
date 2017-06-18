796cce9b9c462679cb574aa4c6238185fe4ff9ba
Pradeep8 <Pradeep8@Pradeep>
2017-06-18 14:21:01 +0530

    Framework using propertry files

--- src/test/HomePageTestcase.java
+++ src/test/HomePageTestcase.java
@@ -1,6 +1,10 @@
 package test;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -52,9 +56,53 @@ public class HomePageTestcase extends BaseRunner
 
 	}
 	
+	/**
+	 * @throws InterruptedException 
+	 * 
+	 */
 	@Test
-	public void clickFriend()
+	public void verifySearchFunctionality() throws InterruptedException
 	{
-		
+	
+	      webdriverGenarics.enterText(driver, "xpath", "//input[@class='_1frb']", "selenium");
+	      webdriverGenarics.click(driver, "xpath", "(//button[@type='submit'])[1]");
+	      
+	      //click see all
+	      
+	      webdriverGenarics.click(driver, "xpath", "(//div[@class='_5dw9 _5dwa _4-u3']/span[2]/a)[1]");
+	      
+	      // list of webelements
+	      List<WebElement> lists = driver.findElements(By.xpath("//div[@class='_52eh _5bcu']"));
+	      System.out.println(lists.size());
+	      for (WebElement Element : lists) 
+	      {
+	    	  String ActualValue=Element.getText();
+	    	  if(ActualValue.equals("Share Selenium"))
+	    	  {
+	    		  Element.click();
+	    		  break;
+	    	  }
+		}
+	      
+	      webdriverGenarics.click(driver, "xpath", "(//div[@class='_4bl9'])[9]");
+	      
+	      webdriverGenarics.click(driver, "classname", "_50f4");
+	      
+	      //webdriverGenarics.switchToParentWindow(driver, "Parentwindow");
+	      //System.out.println("Parentwindow");
+	      //Thread.sleep(1000);
+	      //webdriverGenarics.moveToElementAndClick(driver, "xpath", "//a[text()='Selenium Webdriver']");
+	      
+	      String Title=webdriverGenarics.getTitle(driver);
+	      
+	      if(Title.equals("Share Selenium - Home"))
+	      {
+	    	  System.out.println("Title is :" + Title);
+	    	webdriverGenarics.back(driver);
+	      }
 	}
+	
+	
+	
+	
 }
--- src/testRunner/BaseRunner.java
+++ src/testRunner/BaseRunner.java
@@ -19,7 +19,7 @@ public class BaseRunner
 	
 	@Parameters({"Browser"})
 	@BeforeTest
-	public void appLogin(String browser )
+	public void appLogin(String browser ) throws InterruptedException
 	{
 	driver=webdriverGenarics.launchBrowser(driver, browser, "https://www.facebook.com/");
 	System.out.println(driver);
@@ -33,15 +33,15 @@ public class BaseRunner
 	//submit
 	webdriverGenarics.click(driver, "xpath", "//input[@value='Log In']");
 	
-	
+	Thread.sleep(10000);
 	}
-
+/*
 	@AfterTest
 	public void appQuit()
 	{
 		webdriverGenarics.close(driver);
 	}
-
+*/
 
 	@AfterSuite
 	public void AfterSuite()
